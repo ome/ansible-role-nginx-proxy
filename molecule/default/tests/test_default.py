30d7e347e007fd7faa6f5c22b1f8178ca84c1d0f
Sebastien Besson <seb.besson@gmail.com>
2019-02-21 15:47:47 +0000

    Migrate Molecule 1 to Molecule 2

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,54 @@
 import os
-
+import pytest
 import testinfra.utils.ansible_runner
+
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_service_running_and_enabled(host):
+    assert host.service('nginx').is_running
+    assert host.service('nginx').is_enabled
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+@pytest.mark.parametrize("uri,expecturi,expectcode", [
+    ('/map?search=abc1', '/redirectmap?query=def2', 302),
+    ('/map?search=other', '/redirectmap', 302),
+    ('/direct/', '/redirect', 301),
+    ('/redirect/', '/default', 302),
+    ('/map', '/redirectmap', 302),
+    ('/default', '/default/', 301),
+])
+def test_redirects(host, uri, expecturi, expectcode):
+    out = host.check_output("curl -I http://localhost%s" % uri)
+    assert ('HTTP/1.1 %d' % expectcode) in out
+    assert ('Location: http://localhost%s' % expecturi) in out
+
+
+def test_get_alias(host):
+    out = host.check_output("curl http://localhost/default/")
+    assert '<title>Welcome to nginx!</title>' in out
+
+
+@pytest.mark.parametrize("method,expectcode", [
+    ('GET', 200),
+    ('POST', 403),
+])
+def test_proxy_limit_method(host, method, expectcode):
+    out = host.check_output(
+        "curl -I -X %s -H 'Host: other' http://localhost/limitget/" % method)
+    assert ('HTTP/1.1 %d' % expectcode) in out
+
+
+@pytest.mark.parametrize("path", [
+    'nginx.conf',
+    'conf.d',
+    'stream-conf.d',
+])
+def test_compare_config(host, path):
+    # Check the exit code separately so that the diff is printed out first
+    c = host.run('diff -Nur /root/etc-nginx/%s /etc/nginx/%s' % (
+        path, path))
+    assert c.stdout == ''
+    assert c.rc == 0
