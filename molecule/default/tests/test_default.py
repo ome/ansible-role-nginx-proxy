f465301416c80b4f286d6d72bfefac4c3b86f1a1
Jean-Marie Burel <j.burel@dundee.ac.uk>
2024-01-19 12:38:36 +0000

    revert test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -46,10 +46,10 @@ def test_proxy_limit_method(host, method, expectcode):
     'conf.d',
 ])
 def test_compare_config(host, path):
-    # Check the exit code separately so that the diff is printed out first
-    c = host.run('cat /root/etc-nginx/%s' % path)
-    c1 = host.run('cat /etc/nginx/%s' % path)
-    assert c.stdout == c1.stdout
+    c = host.run('diff -Nur /root/etc-nginx/%s /etc/nginx/%s' % (
+        path, path))
+    assert c.stdout == ''
+    assert c.rc == 0
 
 
 @pytest.mark.skip(reason="Maintenance page no longer works - needs review")
